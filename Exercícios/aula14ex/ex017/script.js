d2dfca9e2f3e3e9f904b9357699d422f3af0e4de
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2023-05-01 15:05:16 -0300

    Módulo E Exercício 17

--- Exercícios/aula14ex/ex017/script.js
+++ Exercícios/aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert("Olá")
+function tabuada() {
+  let num = document.getElementById("txtn")
+  let tab = document.getElementById("seltab")
+  if (num.value.length == 0) {
+    window.alert("Por favor, digite um número!")
+  } else {
+    let n = Number(num.value)
+    let c = 1
+    tab.innerHTML = "" //apagar tabuadas anteriores
+    while (c <= 10) {
+      let item = document.createElement("option") //criar elemento <option> dentro de <select>
+      item.text = `${n} x ${c} = ${n * c}`
+      item.value = `tab ${c}`
+      tab.appendChild(item) //add elemento filho
+      c++
+    }
+  }
+}
